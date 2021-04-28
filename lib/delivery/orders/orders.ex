35311672b50d37b7cb2e6a82b88155465b5e198e
Samuel Silva <catskillet.samuel@socialbank.com.br>
2021-04-27 23:53:53 -0300

    feat: adiciona query list orders

--- lib/delivery/orders/orders.ex
+++ lib/delivery/orders/orders.ex
@@ -8,6 +8,10 @@ defmodule Delivery.Orders do
   alias Delivery.Orders.Order
   alias Delivery.Repo
 
+  def list_orders do
+    Repo.all(Order)
+  end
+
   def create_order(attrs \\ %{}) do
     %Order{}
     |> Order.changeset(attrs)
