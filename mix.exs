9d277e3f3e1d7911c30fad0d807ac771f77ef4a6
Samuel Silva <catskillet.samuel@socialbank.com.br>
2021-04-28 09:50:39 -0300

    feat: adiciona libs tesla e poison

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Delivery.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:timex, "~> 3.0"}
+      {:tesla, "~> 1.4.0"},
+      {:poison, "~> 3.1"},
     ]
   end
 
